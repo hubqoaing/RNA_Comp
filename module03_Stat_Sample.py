529192bcd3f5bf2a6b701453ac39344324b8c94b
huboqiang <huboqiang@gmail.com>
2015-05-06 23:08:31 +0800

    updates

--- module03_Stat_Sample.py
+++ module03_Stat_Sample.py
@@ -266,3 +266,245 @@ for i in `cut -f 1 %s/novo_lnc_raw.combined.FPKM0.5_rep0.25.multiExon.genlen | u
       RepCnt = m_repcnt.RepeatCount( self.repeatCount,l_brief_samp, self.repeat_mrg )
       RepCnt.generate_mat()
       RepCnt.element_group_subgroup_FPKM_sum(np_align_reads)
+      
+      
+   def ERCC_Stat(self):
+      """
+      Using ERCC mols to estimate the total amount of refGene RNA-mols in a cell. 
+      (ERCC_MOLs) / (ERCC_FPKM) = (mRNA_MOLs) / (mRNA_FPKM)
+      """
+      self.l_ERCC_name = []
+      self.l_RGCs_name = []
+      self.l_mRNA_name = []
+      self.l_ERCC_FPKM = {}
+      self.l_RGCs_FPKM = {}
+      self.l_mRNA_FPKM = {}
+      self.l_cirRNA_FPKM={}
+
+      self.l_ERCC_HTSname = []
+      self.l_RGCs_HTSname = []
+      self.l_mRNA_HTSname = []
+      self.l_ERCC_RPKM = {}
+      self.l_RGCs_RPKM = {}
+      self.l_mRNA_RPKM = {}
+
+
+      self.l_ERCC_MOLs = {}
+      self.l_RGCs_MOLs = {}
+      self.l_mRNA_MOLs = {}
+      self.l_cirRNA_MOLs={}
+      self.l_mRNA_MOLs_HTSname = {}
+
+      
+      self.regression = {}
+      
+      self.__load_FPKM()
+      self.__load_MOLs()      # ERCC RGC mols
+      self.__get_mRNA_MOLs()  # get mRNA mols using ERCC_FPKM, ERCC_MOLs and mRNA_FPKM
+      self.__load_Count()
+      
+      out_file   = "%s/02.ERCC_Mols.xls" % (self.statInfo)
+      f_out_file = open( out_file,"w" )
+      
+      out_info = "Sample\tBrief_samp\tERCC_MOLs\tRGC_MOLs\tmRNA_MOLs\tRefSeq_mRNA_MOLs\tRegression_R\tRegression_P\tRefSeq_mRNA_FPKM>0.1"
+      print >>f_out_file, out_info
+      for samp in self['sample']:
+         brief_name = self['sam_info']['samp_brief'][samp]
+         ERCC_MOLs  = sum(    self.l_ERCC_MOLs[  brief_name ] )
+         RGC_MOLs   = sum(    self.l_RGCs_MOLs[  brief_name ] )
+         mRNA_MOLs  = np.sum( self.l_mRNA_MOLs[  brief_name ] )
+         RefSeq_mRNA_MOLs           = np.sum(  self.l_mRNA_MOLs[ brief_name ][ self.mRNA_refSeq_index ] )
+         RefSeq_mRNA_lFPKM          = np.array(self.l_mRNA_FPKM[ brief_name ],dtype=float )
+         RefSeq_mRNA_lFPKM          = RefSeq_mRNA_lFPKM[ self.mRNA_refSeq_index ] 
+         RefSeq_mRNA_Exps           = np.shape(  RefSeq_mRNA_lFPKM[RefSeq_mRNA_lFPKM >= 0.1] )[0]
+         regression_R               = self.regression[brief_name]['r_value']
+         regression_P               = self.regression[brief_name]['p_value']
+         
+         out_info = "%s\t%s\t%6.3e\t%6.3e\t%6.3e\t%6.3e\t%1.3f\t%1.3e\t%d" % ( samp,brief_name, ERCC_MOLs,RGC_MOLs,mRNA_MOLs, RefSeq_mRNA_MOLs,regression_R,regression_P,RefSeq_mRNA_Exps )
+         print >>f_out_file, out_info
+      
+      f_out_file.close()
+      
+   
+   def Repeat_Stat(self):
+
+      l_brief_samp = [ self['sam_info']['samp_brief'][samp] for samp in self['sample'] ]
+     
+      l_align_reads = []
+      for brief_samp in l_brief_samp:
+         Tophat_log  =  "%s/%s/align_summary.txt"           % ( self.tophat,brief_samp )
+         MapStat_info= Stat.TophatStat( Tophat_log )
+         MapStat_info.read_infile()
+         l_align_reads.append( MapStat_info['statInfo']['mappedRead'] )
+         
+      np_align_reads = np.array( l_align_reads )
+     
+      RepCnt = m_repcnt.RepeatCount( self.repeatCount,l_brief_samp, self.repeat_mrg )
+#      RepCnt.generate_mat()
+      RepCnt.element_group_subgroup_FPKM_sum(np_align_reads)
+
+   
+   def __load_Count(self):  # for ERCC count Stat
+      l_breif_samp = [ self['sam_info']['samp_brief'][samp] for samp in self['sample'] ]
+      
+      genome_gtf = self['infile']['anno_file']
+      Gtf_Info = m_gtf.GTFFeature( genome_gtf  )
+      
+      ERCC_info = m_cnt.CountInfo( self.HTS_k,l_breif_samp,"dexseq_ERCC_RGCPloyA",self.HTS )
+      ERCC_info.load_mat()
+      ERCC_info.cal_RPKM( Gtf_Info.gene,self.tophat )
+      
+      
+      
+            
+   def __load_FPKM(self):
+      Cuffnorm_file = "%s/genes.fpkm_table" % ( self.cuffnorm_ercc )
+      f_Cuffnorm    = open( Cuffnorm_file,"r" )
+      in_head        = f_Cuffnorm.readline()
+      f_head         = in_head.split()
+      l_samp_brief   = [ sam.split("_0")[0] for sam in f_head[1:] ]
+      
+      for brief_name in l_samp_brief:
+         self.l_ERCC_FPKM[brief_name] = []
+         self.l_RGCs_FPKM[brief_name] = []
+         self.l_mRNA_FPKM[brief_name] = []
+      
+      for line in f_Cuffnorm:
+         line = line.strip('\n')
+         f    = line.split()
+         gene = f[0]
+         
+         if gene[0:3] == "MIR":
+            continue
+         if gene[0:5] == "SNORD":
+            continue
+         
+         if gene[0:5] == "ERCC-":
+            self.l_ERCC_name.append( gene )
+            for i,brief_name in enumerate( l_samp_brief ):
+               self.l_ERCC_FPKM[brief_name].append( f[i+1] )
+               
+         elif gene[0:4] == "RGC-":
+            self.l_RGCs_name.append( gene )
+            for i,brief_name in enumerate( l_samp_brief ):
+               self.l_RGCs_FPKM[brief_name].append( f[i+1] )
+         
+         else:
+            self.l_mRNA_name.append( gene )
+            for i,brief_name in enumerate( l_samp_brief ):
+               self.l_mRNA_FPKM[brief_name].append( f[i+1] )
+      
+      f_Cuffnorm.close()
+      
+      self.__mRNA_refSeq_index()   
+   
+   def __mRNA_refSeq_index(self):
+      self.mRNA_refSeq_index = []
+      for i,gene in enumerate(self.l_mRNA_name):
+         if gene[0:7] == "NONHSAG" or gene[0:5] == "XLOC_" or gene[0:9] == "novoXLOC_":
+            continue
+         self.mRNA_refSeq_index.append( i )
+      self.mRNA_refSeq_index = np.array( self.mRNA_refSeq_index,dtype=int )
+   
+   def __load_MOLs(self):
+      for samp in self['sample']:
+
+         brief_name = self['sam_info']['samp_brief'][samp]
+         if brief_name not in self.l_ERCC_MOLs:
+            self.l_ERCC_MOLs[brief_name] = []
+         if brief_name not in self.l_RGCs_MOLs:
+            self.l_RGCs_MOLs[brief_name] = []
+            
+         for ERCC_id in self.l_ERCC_name:
+            self.l_ERCC_MOLs[brief_name].append( self.ERCC_info['mol'][ERCC_id]*(6.02*10**23/10**18)*self['sam_info']['dilute'][samp] )
+         
+         self.l_RGCs_MOLs[ brief_name ].append( self['sam_info']['CRE_mols'][ samp ] )
+         self.l_RGCs_MOLs[ brief_name ].append( self['sam_info']['GFP_mols'][ samp ] )
+         self.l_RGCs_MOLs[ brief_name ].append( self['sam_info']['RFP_mols'][ samp ] )
+         
+   def __get_mRNA_MOLs(self):
+      self.regression = {}
+      for samp in self['sample']:
+         brief_name = self['sam_info']['samp_brief'][samp]
+         
+         np_ERCC_FPKM = np.array( self.l_ERCC_FPKM[brief_name],dtype=float )
+         np_ERCC_MOLs = np.array( self.l_ERCC_MOLs[brief_name],dtype=float )
+         
+         np_ERCC_FPKM = np.log2( np_ERCC_FPKM+0.1 )
+         np_ERCC_MOLs = np.log2( np_ERCC_MOLs )
+
+         mol_idx = ( np_ERCC_MOLs - np.log2(6.02*10**23/10**18) > -18 ) * (np_ERCC_FPKM>0)
+
+         np_ERCC_FPKM = np_ERCC_FPKM[ mol_idx ]
+         np_ERCC_MOLs = np_ERCC_MOLs[ mol_idx ]
+         
+         slope,intercept,r_value,p_value,slope_std_error =  scipy.stats.linregress( np_ERCC_MOLs,np_ERCC_FPKM )
+         
+         self.regression[brief_name] = {'slope'    :slope,           \
+                                        'inter'    :intercept,       \
+                                        'r_value'  :r_value,         \
+                                        'p_value'  :p_value,         \
+                                        'std_err'  :slope_std_error  \
+         }
+         
+         np_mRNA_FPKM = np.array( self.l_mRNA_FPKM[brief_name],dtype=float )
+         np_mRNA_FPKM = np.log2( np_mRNA_FPKM+0.1 )
+         np_mRNA_MOLs = ( np_mRNA_FPKM - intercept ) / slope
+         np_mRNA_MOLs = np.power( np_mRNA_MOLs, 2 )
+         np_mRNA_MOLs[ np_mRNA_MOLs<0.0            ] = 0.0
+         np_mRNA_MOLs[ np_mRNA_FPKM<=np.log2(0.1) ] = 0.0
+         
+         self.l_mRNA_MOLs[ brief_name ] = np_mRNA_MOLs   
+   
+   
+   def plot_regression(self):
+      pdfname = "All_samples.rpkm_cnt.pdf" 
+      fig = plt.figure(figsize=(150,150))
+      cnt = 0
+      for samp in self['sample']:
+         brief_name = self['sam_info']['samp_brief'][samp]
+         
+         slope          =  self.regression[brief_name]['slope']
+         intercept      =  self.regression[brief_name]['inter']
+         r_value        =  self.regression[brief_name]['r_value']
+         p_value        =  self.regression[brief_name]['p_value']
+         slope_std_error=  self.regression[brief_name]['std_err']
+         
+         np_ERCC_FPKM = np.array( self.l_ERCC_FPKM[brief_name],dtype=float )
+         np_ERCC_MOLs = np.array( self.l_ERCC_MOLs[brief_name],dtype=float )/(6.02*10**23/10**18)
+      
+         np_ERCC_FPKM = np.log2( np_ERCC_FPKM+0.1 )
+         np_ERCC_MOLs = np.log2( np_ERCC_MOLs )
+
+         mol_idx = ( np_ERCC_MOLs > -18 ) * (np_ERCC_FPKM>0)
+
+         np_ERCC_FPKM_use = np_ERCC_FPKM[ mol_idx ]
+         np_ERCC_MOLs_use = np_ERCC_MOLs[ mol_idx ]
+         
+         MOLs_predict = np.linspace( -18,-5,100 )
+         FPKM_predict = intercept + slope * (MOLs_predict + np.log2(6.02*10**23/10**18) )
+      
+         cnt += 1
+         ax = plt.subplot( 20,20,cnt)
+         ax.plot( np_ERCC_MOLs,     np_ERCC_FPKM      ,".r")
+         ax.plot( np_ERCC_MOLs_use, np_ERCC_FPKM_use  ,"ro")
+         ax.plot( MOLs_predict,     FPKM_predict           )
+         title = brief_name + ' ERCC-mols vs RPKM in samples'
+         ax.set_title(title,fontsize=12)
+         ax.set_xlabel('Spike-in mols per reaction(attlmole,log2)')
+         ax.set_ylabel('Mean read-coverage of spike-in molecules(RPKM,log2)')
+         ax.text(-18,13.5,r'$ y = %f + %fx $' % (intercept,slope))
+         ax.text(-18,13.0,r'$ Pearson R = %f $' % (r_value))
+         ax.text(-18,12.5,r'$ p = %6.2e $' % (p_value))
+         ax.text(-18,12.0,r'$ mRNA genes = %6.2e $' % (  np.sum( self.l_mRNA_MOLs[ brief_name ][ self.mRNA_refSeq_index ] )  ) )
+         for tick in ax.xaxis.get_major_ticks():
+            tick.tick1On = True
+            tick.tick2On = False
+         for tick in ax.yaxis.get_major_ticks():
+            tick.tick1On = True
+            tick.tick2On = False
+#         ax.set_xlim(-20,-5)
+#         ax.set_ylim(-4,15)
+         
+      plt.savefig(pdfname,format='pdf')
+   
